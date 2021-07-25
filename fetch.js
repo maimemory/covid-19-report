7d17c58eb8e1dae48e57ed849f1599c6e64f2907
maimemory <jumpol@venussolutions.com>
2021-07-25 21:07:52 +0700

    add CSS

--- fetch.js
+++ fetch.js
@@ -1,17 +1,14 @@
 $(document).ready(function () {
-  fetch("https://covid19.th-stat.com/json/covid19v2/getTodayCases.json").then(
-    (result) => {
+  fetch("https://covid19.th-stat.com/json/covid19v2/getTodayCases.json")
+    .then((result) => {
       console.log(result);
-    //   const text = JSON.stringify(result);
-    //   $("#info").append(text);
-    return result.json();
-    }
-  )
-  .then((data) => {
+      return result.json();
+    })
+    .then((data) => {
       console.log(data);
       const newCases = JSON.stringify(data.NewConfirmed);
       const newDeaths = JSON.stringify(data.NewDeaths);
-      $("#info").append(newCases, "<br>", newDeaths);
-  })
-
+      $("#newCase").text(newCases);
+      $("#newDeath").text(newDeaths);
+    });
 });
