0b02eb52db88e6f518d34aa4ebbfb5d173e7346a
maimemory <jumpol@venussolutions.com>
2021-07-30 23:02:57 +0700

    Add Update Date and Dark Mode Display

--- fetch.js
+++ fetch.js
@@ -14,5 +14,20 @@ $(document).ready(function () {
       $("#newDeath").text(newDeaths);
       $("#totalCase").text(totalCases);
       $("#totalDeath").text(totalDeaths);
+      $("#updateDate").text(data.UpdateDate);
     });
 });
+
+
+let toggle = document.querySelector("#toggle");
+
+const darkMode = () => {
+  if(toggle.checked == true){
+    $("p").css("color", "#ffffff");
+    $("body").css("background-color", "rgb(32, 32, 32)");
+  }
+  else{
+    $("p").css("color", "#000000");
+    $("body").css("background-color", "#ffffff");
+  }
+};
